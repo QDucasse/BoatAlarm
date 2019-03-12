55d3c9fb4d91f02ce42c2b081671078ac6c96b00
Quentin Ducasse <quentin.ducasse@free.fr>
2019-03-12 09:29:14 +0100

    Refactoring

--- src/users/Subscriber.java
+++ src/users/Subscriber.java
@@ -6,22 +6,37 @@ import java.util.List;
 import boat.Boat;
 
 
-public class Subscriber extends User {
+public class Subscriber {
 	//==================
 	//Instance Variables
 	
+	private String account;
+	private String password;
+	private String name;
+	private String address;
+	private String email;
 	private Date subscription_date;
-	//List<String> Emails = new ArrayList<String>();
-	private List<User> trusted_persons = new ArrayList<User>();
-	Boat boat;
+	private List<String> emails = new ArrayList<String>();
+	private List<Confidence> trusted_persons = new ArrayList<Confidence>();
+	private Boat boat;
+
+	
 	//==================
 	//Constructors
 	
-	public Subscriber(String person_name, String person_address, String person_email, 
-					  int boat_immatriculation, String boat_name, String boat_type, 
-					  String boat_model)
+	public Subscriber(String account,String password,
+					  String name, String address, String email, 
+					  Date date, Confidence conf_person,
+					  int boat_immatriculation, String boat_name, 
+					  String boat_type, String boat_model)
 	{
-		super(person_name,person_address,person_email);
+		this.account	       = account;
+		this.password	       = password;
+		this.name			   = name;
+		this.address		   = address;
+		this.trusted_persons.add(conf_person);
+		this.emails.add(email);
+		//this.subscription_date = date;
 		this.subscription_date = new Date();
 				// display time and date
 			     // System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:", date);
@@ -31,6 +46,46 @@ public class Subscriber extends User {
 	//==================
 	//Getters and Setters
 
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public Date getSubscription_date() {
 		return subscription_date;
 	}
@@ -39,11 +94,19 @@ public class Subscriber extends User {
 		this.subscription_date = subscription_date;
 	}
 
-	public List<User> getTrusted_persons() {
+	public List<String> getEmails() {
+		return emails;
+	}
+
+	public void setEmails(List<String> emails) {
+		this.emails = emails;
+	}
+
+	public List<Confidence> getTrusted_persons() {
 		return trusted_persons;
 	}
 
-	public void setTrusted_persons(List<User> trusted_persons) {
+	public void setTrusted_persons(List<Confidence> trusted_persons) {
 		this.trusted_persons = trusted_persons;
 	}
 
@@ -55,15 +118,6 @@ public class Subscriber extends User {
 		this.boat = boat;
 	}
 
-	/*
-	public String getEmail() {
-		return Email;
-	}
-	public void setEmail(String Email) {
-		this.Email = Email;
-	}
-	*/
-
 	//==================
 	//Other Methods
 	
@@ -71,6 +125,7 @@ public class Subscriber extends User {
 	public String toString() {
 		return "Abonne [name=" + name + ", address=" + address + ", email=" + email + ", subscription_date=" + subscription_date + ", boat=" + boat + "]";
 	}
+
 
 
 	
